1030db81b1bd1d19a033f24c2bbb0f340cd1600f
Leonan <pedroleonan85@gmail.com>
2024-09-22 18:16:22 -0300

    Correção de versão e arquivos de configuração

--- app/src/main/java/com/example/planup/ui/screens/LoginScreen.kt
+++ app/src/main/java/com/example/planup/ui/screens/LoginScreen.kt
@@ -1,7 +1,5 @@
 package com.example.planup.ui.screens
 
-import android.util.Log
-import android.widget.Toast
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
@@ -33,7 +31,6 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -43,7 +40,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
@@ -54,7 +50,6 @@ import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import com.example.planup.R
 import com.example.planup.auth.EmailAndPasswordAuth
-import com.example.planup.auth.SignInState
 import kotlinx.coroutines.launch
 
 @Composable
