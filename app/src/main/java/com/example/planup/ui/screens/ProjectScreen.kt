0be512929f9a3df6ba6812036854b50476dc55ae
Bowie <marketing@letscode.com.br>
2024-09-24 17:36:32 -0300

    HU.39 - Inicialização da expanção do projeto.

--- app/src/main/java/com/example/planup/ui/screens/ProjectScreen.kt
+++ app/src/main/java/com/example/planup/ui/screens/ProjectScreen.kt
@@ -1,13 +1,313 @@
 package com.example.planup.ui.screens
 
-import android.widget.Toast
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.filled.AddCircle
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.icons.rounded.MoreVert
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.CheckboxDefaults
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.LinearProgressIndicator
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.platform.LocalContext
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import com.example.planup.model.Project
+import com.example.planup.model.ProjectViewModel
 
 @Composable
-fun ProjectScreen(navController: NavHostController, project: Project) {
-    val context = LocalContext.current
-    Toast.makeText(context, "Project: ${project.name}", Toast.LENGTH_SHORT).show()
+fun ProjectScreen(navController: NavHostController? = null, project: Project? = null) {
+    val viewModel: ProjectViewModel = viewModel()
+    var progress by remember { mutableFloatStateOf(0.0f) }
+    var numTarefas by remember { mutableIntStateOf(0) }
+    var tarefasConcluidas by remember { mutableIntStateOf(0) }
+    var check by remember { mutableStateOf(false) }
+
+    val backgroundImage: Painter = painterResource(viewModel.getImageForElement(project!!.name))
+    val backgroundSize = 200.dp
+
+    val color = viewModel.getColorForElement(project.name)
+
+    Scaffold(
+        modifier = Modifier.fillMaxSize()
+    ) { innerPadding ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding)
+                .background(Color(0XFF181A20)),
+        ) {
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(backgroundSize)
+                    .background(Color.Gray)
+            ) {
+                Image(
+                    painter = backgroundImage,
+                    contentDescription = null,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(backgroundSize),
+                    contentScale = ContentScale.Crop
+                )
+
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                ) {
+                    Spacer(modifier = Modifier.height(10.dp))
+
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(20.dp, 0.dp, 20.dp, 0.dp),
+                        horizontalArrangement = Arrangement.SpaceBetween
+                    ) {
+                        IconButton(
+                            onClick = {
+                                navController!!.popBackStack()
+                            }
+                        ) {
+                            Icon(
+                                imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                                contentDescription = "Voltar",
+                                tint = Color.White,
+                                modifier = Modifier.size(20.dp)
+                            )
+                        }
+
+                        IconButton(
+                            onClick = {
+                                /*TODO*/
+                            }
+                        ) {
+                            Icon(
+                                imageVector = Icons.Filled.Search,
+                                contentDescription = "Pesquisar",
+                                tint = Color.White,
+                                modifier = Modifier.size(20.dp)
+                            )
+                        }
+
+                        IconButton(
+                            onClick = {
+                                /*TODO*/
+                            }
+                        ) {
+                            Icon(
+                                imageVector = Icons.Rounded.MoreVert,
+                                contentDescription = "Outros",
+                                tint = Color.White,
+                                modifier = Modifier.size(20.dp)
+                            )
+                        }
+                    }
+                }
+            }
+
+            Spacer(modifier = Modifier.height(20.dp))
+
+            Text(
+                text = project.name,
+                fontSize = 24.sp,
+                color = Color.White,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier.padding(20.dp, 0.dp, 0.dp, 0.dp)
+            )
+
+            Spacer(modifier = Modifier.height(10.dp))
+
+            project.description?.let {
+                Text(
+                    text = project.description,
+                    fontSize = 12.sp,
+                    fontWeight = FontWeight.SemiBold,
+                    color = Color.LightGray,
+                    modifier = Modifier.padding(20.dp, 0.dp, 0.dp, 0.dp)
+                )
+            }
+
+            Spacer(modifier = Modifier.height(20.dp))
+
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(20.dp, 0.dp, 0.dp, 0.dp),
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(
+                    text = project.owner,
+                    fontSize = 10.sp,
+                    fontWeight = FontWeight.Bold,
+                    color = Color.LightGray
+                )
+
+                IconButton(
+                    onClick = {
+                        navController!!.navigate("create_task_screen")
+                    }
+                ) {
+                    Icon(
+                        imageVector = Icons.Filled.AddCircle,
+                        contentDescription = "Adicionar nova tarefa",
+                        tint = Color.White,
+                        modifier = Modifier.size(25.dp)
+                    )
+                }
+            }
+
+            Spacer(modifier = Modifier.height(20.dp))
+
+            Box(modifier = Modifier.padding(20.dp, 0.dp, 0.dp, 0.dp)) {
+                Box(
+                    modifier = Modifier
+                        .wrapContentSize()
+                        .background(Color(color.value), shape = RoundedCornerShape(20.dp))
+                ) {
+                    Text(
+                        text = "$tarefasConcluidas / $numTarefas",
+                        modifier = Modifier.padding(15.dp, 2.dp,15.dp, 2.dp),
+                        fontSize = 10.sp,
+                        color = Color.White,
+                        fontWeight = FontWeight.Light
+                    )
+                }
+            }
+
+            Spacer(modifier = Modifier.height(10.dp))
+
+            LinearProgressIndicator(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(20.dp, 0.dp, 20.dp, 0.dp),
+                progress = { progress },
+                color = Color(color.value),
+                trackColor = Color(0XFF35383F),
+            )
+
+            Spacer(modifier = Modifier.height(20.dp))
+
+            project.taskLists?.let { taskList ->
+                LazyColumn(
+                    modifier = Modifier
+                        .fillMaxWidth(),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    items(taskList.size) { task ->
+                        Box(
+                            modifier = Modifier
+                                .fillMaxWidth(0.8f)
+                                .wrapContentHeight()
+                                .padding(10.dp)
+                                .background(Color(0XFF35383F))
+                        ) {
+                            Column(
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .wrapContentHeight()
+                            ) {
+                                Text(
+                                    text = taskList[task].name,
+                                    fontSize = 16.sp,
+                                    fontWeight = FontWeight.Bold,
+                                    color = Color.White
+                                )
+
+                                Text(
+                                    text = taskList[task].description,
+                                    fontSize = 12.sp,
+                                    fontWeight = FontWeight.Medium,
+                                    color = Color.LightGray
+                                )
+
+                                Row(
+                                    modifier = Modifier
+                                        .fillMaxWidth(),
+                                    verticalAlignment = Alignment.CenterVertically,
+                                    horizontalArrangement = Arrangement.SpaceBetween
+                                ) {
+                                    Text(
+                                        text = "Tarefa concluída",
+                                        fontSize = 12.sp,
+                                        fontWeight = FontWeight.Medium,
+                                        color = Color.LightGray
+                                    )
+
+                                    Checkbox(
+                                        checked = check,
+                                        onCheckedChange ={
+                                            check = it
+                                        },
+                                        modifier = Modifier.size(16.dp),
+                                        colors = CheckboxDefaults.colors(
+                                            checkedColor = Color(0XFF246BFD),
+                                            uncheckedColor = Color(0XFF35383F)
+                                        )
+                                    )
+                                }
+
+                            }
+                        }
+
+                        numTarefas = taskList.size
+                        if(check) tarefasConcluidas++ else if(tarefasConcluidas > 0) tarefasConcluidas--
+                        progress = tarefasConcluidas.toFloat() / numTarefas.toFloat()
+
+                        Spacer(modifier = Modifier.height(10.dp))
+                    }
+                }
+            } ?: run {
+                CircularProgressIndicator(
+                    modifier = Modifier
+                        .align(Alignment.CenterHorizontally)
+                        .size(50.dp),
+                    color = Color.White
+                )
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun ProjectScreenPreview() {
+    ProjectScreen()
 }
