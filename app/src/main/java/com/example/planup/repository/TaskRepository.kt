b48684282db64aa6a3bb47f606235644ed2022c8
aquilesSou22 <aqlsnns@gmail.com>
2024-09-24 18:34:26 -0300

    HU 05 - botão modal integrado ao BD (versão 0.1)

--- app/src/main/java/com/example/planup/repository/TaskRepository.kt
+++ app/src/main/java/com/example/planup/repository/TaskRepository.kt
@@ -5,12 +5,29 @@ import com.example.planup.model.Task
 import com.example.planup.network.RetrofitInstance
 import okhttp3.ResponseBody
 import retrofit2.Call
-import retrofit2.Callback
 import retrofit2.Response
+import retrofit2.Callback
 
 class TaskRepository {
 
     private val apiService = RetrofitInstance.apiService
+
+    fun deleteTask(taskId: String) {
+        apiService.deleteTask(taskId).enqueue(object: Callback<ResponseBody> {
+            override fun onResponse(call: Call<ResponseBody>, response: Response<ResponseBody>) {
+                if(response.isSuccessful){
+                    Log.d("DeleteTask", "Tarefa deletada com sucesso: ${response.body()}")
+                } else {
+                    Log.d("DeleteTask", "Falha ao deletar tarefa: ${response.errorBody()?.string()}")
+                }
+            }
+
+            override fun onFailure(call: Call<ResponseBody>, t: Throwable) {
+                    Log.e("DeleteTask", "Erro ao deletar tarefa: ${t.message}")
+                    t.printStackTrace()
+            }
+        })
+    }
 
     fun postTasks(task: Task) {
         apiService.postTask(task).enqueue(object : Callback <ResponseBody> {
